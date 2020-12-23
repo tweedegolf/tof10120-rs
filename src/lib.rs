d2e010da8b8354c3f4945b49ecde2e60db49c04f
Henk Dieter Oordt <henkdieter@oordt.net>
2020-12-23 13:36:07 +0100

    Add some comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 #![no_std]
+//! Simple driver for the TOF10120 time-of-flight distance sensor.
+//! For now, only implements communication over I2C.
+//! This time-of-flight sensor yields 16-bit values in millimeters.
 
 use core::marker::PhantomData;
 use embedded_hal::blocking::i2c::WriteRead;
 const TOF10120_ADDR: u8 = 0x52; //( == 0xa4 & 0x7F)
 
+/// Wrapper for a TOF10120, specific for an I2C peripheral,
+/// so that it cannot be written to from multiple peripherals,
+/// which would not make sense.
 pub struct TOF10120<TI2C> {
     _marker: PhantomData<TI2C>,
 }
@@ -12,7 +18,7 @@ impl<TI2C, E> TOF10120<TI2C>
 where
     TI2C: WriteRead<Error = E>,
 {
-    /// Initialize the driver
+    /// Initialize the driver, binding it to an I2C peripheral
     pub fn init(_i2c: &mut TI2C) -> Self {
         Self {
             _marker: PhantomData,
